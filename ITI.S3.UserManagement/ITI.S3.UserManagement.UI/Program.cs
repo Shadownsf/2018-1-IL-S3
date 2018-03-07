af60eba802702fdaef23d428df27e1a2bf444c20
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-03-07 11:59:56 +0100

    Added ITI.S3.UserManagement.

---
+++ ITI.S3.UserManagement/ITI.S3.UserManagement.UI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ITI.S3.UserManagement.UI
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            User john = new User();
+            john.UserName = "John";
+
+            john.SetPassword( "ù**^$^rzer" );
+            if( john.PasswordMatch( "wrong pass" ) )
+            {
+                Console.WriteLine( "Password matched" );
+            }
+            else
+            {
+                Console.WriteLine( "Password didn't match" );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
