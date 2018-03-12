e9a7305f5537f9cf6cbf0f0fcc25276bdcb227f2
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-03-12 12:08:47 +0100

    Added ITI.S3.Geometry.

---
+++ ITI.S3.Geometry/ITI.S3.Geometry.App/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ITI.S3.Geometry.App
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Segment segment = new Segment( 0, 10 );
+            DisplaySegment( segment );
+
+            segment.Move( 2 );
+            DisplaySegment( segment );
+
+            Console.WriteLine( "Segment (2, 12) contains the point 4: {0}", segment.Contains( 4 ) );
+            Console.WriteLine( "Segment (2, 12) contains the point 12: {0}", segment.Contains( 12 ) );
+            Console.WriteLine( "Segment (2, 12) contains the point 0: {0}", segment.Contains( 12 ) );
+
+            segment = new Segment( 0, 10 );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [0, 10[  : {0} - should be True", segment.Overlaps( new Segment( 0, 10 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [-6, 5[  : {0} - should be True", segment.Overlaps( new Segment( -6, 11 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [8, 15[  : {0} - should be True", segment.Overlaps( new Segment( 8, 7 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [-10, -5[: {0} - should be False", segment.Overlaps( new Segment( -10, 5 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [5, 8[   : {0} - should be True", segment.Overlaps( new Segment( 5, 3 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [11, 16[ : {0} - should be False", segment.Overlaps( new Segment( 11, 5 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [-5, 15[ : {0} - should be True", segment.Overlaps( new Segment( -5, 20 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [10, 20[ : {0} - should be False", segment.Overlaps( new Segment( 10, 10 ) ) );
+            Console.WriteLine( "Segment [0, 10[ overlaps segment [-10, 0[ : {0} - should be False", segment.Overlaps( new Segment( -10, 10 ) ) );
+
+            Segment intersection = segment.Intersection( new Segment( -6, 11 ) );
+            Console.WriteLine("Intersection between [0, 10[ and [-6, 5[:");
+            DisplaySegment( intersection );
+
+            intersection = segment.Intersection( new Segment( 8, 7 ) );
+            Console.WriteLine("Intersection between [0, 10[ and [8, 15[:");
+            DisplaySegment( intersection );
+
+            intersection = segment.Intersection( new Segment( 10, 2 ) );
+            Console.WriteLine("Intersection between [0, 10[ and [10, 12[ is null: {0}", intersection == null);
+        }
+
+        static void DisplaySegment( Segment segment )
+        {
+            Console.WriteLine( "Left: {0}", segment.Left );
+            Console.WriteLine( "Right: {0}", segment.Right );
+            Console.WriteLine( "Length: {0}", segment.Length );
+        }
+    }
+}
---
+++ ITI.S3.Geometry/ITI.S3.Geometry/Segment.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace ITI.S3.Geometry
+{
+    public class Segment
+    {
+        int _position;
+        readonly ushort _length;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Segment"/> class.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="length">The length.</param>
+        public Segment( int position, ushort length )
+        {
+            _position = position;
+            _length = length;
+        }
+
+        /// <summary>
+        /// Gets the left position.
+        /// </summary>
+        /// <value>
+        /// The left.
+        /// </value>
+        public int Left => _position;
+
+        /// <summary>
+        /// Gets the right position.
+        /// </summary>
+        /// <value>
+        /// The right.
+        /// </value>
+        public int Right => _position + _length;
+
+        /// <summary>
+        /// Gets the length of the current <see cref="Segment"/>.
+        /// </summary>
+        /// <value>
+        /// The length.
+        /// </value>
+        public int Length => _length;
+
+        /// <summary>
+        /// Moves the current <see cref="Segment"/>.
+        /// </summary>
+        /// <param name="delta">The delta.</param>
+        public void Move( int delta )
+        {
+            _position += delta;
+        }
+
+        /// <summary>
+        /// Determines whether the current <see cref="Segment"/> contains the specified point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>
+        ///   <c>true</c> if contains the specified point; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Contains( int point )
+        {
+            return Left <= point && point < Right;
+        }
+
+        /// <summary>
+        /// Overlapses the specified <see cref="Segment"/>.
+        /// </summary>
+        /// <param name="other">The other <see cref="Segment"/>.</param>
+        /// <returns></returns>
+        public bool Overlaps( Segment other )
+        {
+            return other.Left < Right && other.Right > Left;
+        }
+
+        /// <summary>
+        /// Gets the intersections with the specified <see cref="Segment"/>.
+        /// </summary>
+        /// <param name="other">The other <see cref="Segment"/>.</param>
+        /// <returns>A new <see cref="Segment"/> that represents the intersection.</returns>
+        public Segment Intersection( Segment other )
+        {
+            if( !Overlaps( other ) ) return null;
+            if( Left <= other.Left ) return new Segment( other.Left, ( ushort )( Right - other.Left ) );
+            return other.Intersection( this );
+        }
+    }
+}
