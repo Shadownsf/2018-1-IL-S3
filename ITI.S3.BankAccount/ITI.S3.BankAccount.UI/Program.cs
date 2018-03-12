adb4c1f5b558a827951aa585148704cfd1bffb25
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-03-12 10:17:34 +0100

    Added ITI.S3.BankAccount.

---
+++ ITI.S3.BankAccount/ITI.S3.BankAccount.UI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ITI.S3.BankAccount.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account( "123456789", "1234", 1000 );
+            int balance = account.GetBalance( "1234" );
+            Console.WriteLine( "Balance: {0}", balance );
+            account.Credit( 2000 );
+            balance = account.GetBalance( "1234" );
+            Console.WriteLine( "Balance: {0}", balance );
+            balance = account.GetBalance( "0000" );
+            Console.WriteLine( "With wrong pass, balance is: {0}", balance );
+
+            Account invalidAccount = new Account( "4321", "0000", -1000 );
+            Console.WriteLine( "Balance: {0}", invalidAccount.GetBalance( "0000" ) );
+
+            account.Debit( "1234", 1500 );
+            balance = account.GetBalance( "1234" );
+            Console.WriteLine( "Balance: {0}", balance );
+
+            account.Debit( "12345", 1500 );
+            balance = account.GetBalance( "1234" );
+            Console.WriteLine( "Balance: {0}", balance );
+
+            account.ChangePassword( "1234", "4321" );
+            Console.WriteLine( "Password changed: {0}", account.GetBalance( "4321" ) == 1500 );
+
+            account.ChangePassword( "0000", "5555" );
+            Console.WriteLine( "Password changed: {0}", account.GetBalance( "5555" ) != -1 );
+        }
+    }
+}
---
+++ ITI.S3.BankAccount/ITI.S3.BankAccount/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ITI.S3.BankAccount
+{
+    /// <summary>
+    /// Represents a bank account.
+    /// </summary>
+    public class Account
+    {
+        readonly string _accountNumber;
+        string _password;
+        int _balance;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Account"/> class.
+        /// </summary>
+        /// <param name="accountNumber">The account number.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="balanceInCents">The balance in cents.</param>
+        public Account( string accountNumber, string password, int balanceInCents )
+        {
+            _accountNumber = accountNumber;
+            _password = password;
+            _balance = Math.Max( 0, balanceInCents );
+        }
+
+        /// <summary>
+        /// Credits the current account.
+        /// </summary>
+        /// <param name="amountInCents">The amount in cents.</param>
+        public void Credit( int amountInCents )
+        {
+            _balance += Math.Max( 0, amountInCents );
+        }
+
+        /// <summary>
+        /// Gets the balance.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        public int GetBalance( string password )
+        {
+            if( password == _password ) return _balance;
+            return -1;
+        }
+
+        /// <summary>
+        /// Debits the current account.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <param name="amountInCents">The amount in cents.</param>
+        public void Debit( string password, int amountInCents )
+        {
+            if( password == _password && amountInCents > 0 && amountInCents <= _balance ) _balance -= amountInCents;
+        }
+
+        /// <summary>
+        /// Changes the password.
+        /// </summary>
+        /// <param name="oldPassword">The old password.</param>
+        /// <param name="newPassword">The new password.</param>
+        public void ChangePassword( string oldPassword, string newPassword )
+        {
+            if( _password == oldPassword ) _password = newPassword;
+        }
+    }
+}
