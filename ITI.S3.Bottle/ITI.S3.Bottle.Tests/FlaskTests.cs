10ee3724e1e594937b0d62ee0f632dbfa9cee786
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-03-14 10:29:57 +0100

    Added Bottle's unit tests.

---
+++ ITI.S3.Bottle/ITI.S3.Bottle.Tests/FlaskTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ITI.S3.Bottle.Tests
+{
+    [TestFixture]
+    public class FlaskTests
+    {
+        [Test]
+        public void fill_a_flask()
+        {
+            Flask sut = new Flask( 2 );
+            sut.Fill( 1 );
+            Assert.AreEqual( 1, sut.Volume );
+        }
+
+        [Test]
+        public void empty_flask()
+        {
+            Flask sut = new Flask( 3 );
+            sut.Fill( 2 );
+
+            sut.Empty( 1 );
+
+            Assert.That( sut.Volume, Is.EqualTo( 1 ) );
+        }
+    }
+}
