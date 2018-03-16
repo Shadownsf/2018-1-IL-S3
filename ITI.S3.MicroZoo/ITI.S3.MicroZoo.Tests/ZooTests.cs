b72c17eea0b2ce89e5464e235f61594abeeff991
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-03-16 10:02:38 +0100

    Added ITI.S3.MicroZoo.

---
+++ ITI.S3.MicroZoo/ITI.S3.MicroZoo.Tests/ZooTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace ITI.S3.MicroZoo.Tests
+{
+    [TestFixture]
+    public class ZooTests
+    {
+        [Test]
+        public void create_some_birds()
+        {
+            Zoo sut = new Zoo();
+
+            Bird jean = sut.CreateBird( "Jean" );
+            Bird pierre = sut.CreateBird( "Pierre" );
+            Bird titi = sut.CreateBird( "Titi" );
+
+            Assert.That( jean.Name, Is.EqualTo( "Jean" ) );
+            Assert.That( pierre.Name, Is.EqualTo( "Pierre" ) );
+            Assert.That( titi.Name, Is.EqualTo( "Titi" ) );
+        }
+
+        [Test]
+        public void find_bird_by_name()
+        {
+            Zoo sut = new Zoo();
+            Bird jean = sut.CreateBird( "Jean" );
+            Bird pierre = sut.CreateBird( "Pierre" );
+            Bird titi = sut.CreateBird( "Titi" );
+
+            Assert.That( sut.FindBird( "Pierre" ), Is.SameAs( pierre ) );
+            Assert.That( sut.FindBird( "Jean" ), Is.SameAs( jean ) );
+            Assert.That( sut.FindBird( "Titi" ), Is.SameAs( titi ) );
+        }
+    }
+}
---
+++ ITI.S3.MicroZoo/ITI.S3.MicroZoo/Zoo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ITI.S3.MicroZoo
+{
+    public class Zoo
+    {
+        readonly Dictionary<string, Bird> _birds;
+
+        public Zoo()
+        {
+            _birds = new Dictionary<string, Bird>();
+        }
+
+        public Bird CreateBird( string name )
+        {
+            Bird bird = new Bird( name );
+            _birds.Add( name, bird );
+            return bird;
+        }
+
+        public Bird FindBird( string name )
+        {
+            return _birds[ name ];
+        }
+    }
+}
