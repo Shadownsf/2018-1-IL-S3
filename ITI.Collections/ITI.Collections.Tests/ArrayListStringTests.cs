f38bb6699d4da2049469cda56dc1819355390e0f
Antoine Raquillet <antoine.raquillet@esiea.fr>
2018-03-23 10:14:18 +0100

    Implements ArrayListString.Add(string).

--- ITI.Collections/ITI.Collections.Tests/ArrayListStringTests.cs
+++ ITI.Collections/ITI.Collections.Tests/ArrayListStringTests.cs
@@ -7,15 +7,39 @@ namespace ITI.Collections.Tests
     public class ArrayListStringTests
     {
         [Test]
-        public void should_pass()
+        public void add_items()
         {
+            ArrayListString sut = new ArrayListString();
+            Assert.That( sut.Count, Is.EqualTo( 0 ) );
 
+            sut.Add( "W" );
+            Assert.That( sut.Count, Is.EqualTo( 1 ) );
+
+            sut.Add( "A" );
+            Assert.That( sut.Count, Is.EqualTo( 2 ) );
+
+            sut.Add( "B" );
+            Assert.That( sut.Count, Is.EqualTo( 3 ) );
+
+            sut.Add( "Z" );
+            Assert.That( sut.Count, Is.EqualTo( 4 ) );
+
+            Assert.That( sut.GetAt( 0 ), Is.EqualTo( "W" ) );
+            Assert.That( sut.GetAt( 1 ), Is.EqualTo( "A" ) );
+            Assert.That( sut.GetAt( 2 ), Is.EqualTo( "B" ) );
+            Assert.That( sut.GetAt( 3 ), Is.EqualTo( "Z" ) );
         }
 
         [Test]
-        public void should_fail()
+        public void resize_arraylist()
         {
-            Assert.That( 0, Is.EqualTo( 25 ) );
+            ArrayListString sut = new ArrayListString();
+            const int Count = 1000;
+
+            for( int i = 0; i < Count; i++ ) sut.Add( string.Format( "Test-{0}", i ) );
+
+            Assert.That( sut.Count, Is.EqualTo( Count ) );
+            for( int i = 0; i < Count; i++ ) Assert.That( sut.GetAt( i ), Is.EqualTo( string.Format( "Test-{0}", i ) ) );
         }
     }
 }
--- ITI.Collections/ITI.Collections/ArrayListString.cs
+++ ITI.Collections/ITI.Collections/ArrayListString.cs
@@ -4,5 +4,28 @@ namespace ITI.Collections
 {
     public class ArrayListString
     {
+        string[] _values;
+        int _count;
+
+        public ArrayListString()
+        {
+            _values = new string[ 4 ];
+        }
+
+        public void Add( string s )
+        {
+            _values[ _count ] = s;
+            _count++;
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public string GetAt( int index )
+        {
+            return _values[ index ];
+        }
     }
 }
